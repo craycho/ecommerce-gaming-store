f4a5021548becb856f8ddb9e41ca33d60cd8c519
craycho <bakir.kreco@gmail.com>
2023-08-11 21:42:16 +0200

    UI Changes + new cart feature

--- src/util/calc-total-price.tsx
+++ src/util/calc-total-price.tsx
@@ -37,31 +37,3 @@ const calcTotalPrice = (cartArray: Product[] = []) => {
 };
 
 export default calcTotalPrice;
-
-/* export function getTotalPrice(
-  onSale: boolean,
-  price: number,
-  amount: number,
-  totalPrice: number,
-  addedToCart: boolean
-) {
-  const productPrice = onSale
-    ? +(price - price * 0.3).toFixed(2)
-    : +price.toFixed(2);
-
-  if (amount > 1) {
-    const finalPrice = +(totalPrice - productPrice * amount).toFixed(2);
-    localStorage.setItem("totalPrice", JSON.stringify(finalPrice));
-    return finalPrice;
-  }
-
-  if (addedToCart) {
-    const finalPrice = +(totalPrice + productPrice).toFixed(2);
-    localStorage.setItem("totalPrice", JSON.stringify(finalPrice));
-    return finalPrice;
-  } else {
-    const finalPrice = +(totalPrice - productPrice).toFixed(2);
-    localStorage.setItem("totalPrice", JSON.stringify(finalPrice));
-    return finalPrice;
-  }
-} */
